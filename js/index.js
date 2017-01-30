605b32be8717fb98ef509b61953b33f6024bdddc
Mykhailo <staskopernik@gmail.com>
2017-01-30 13:38:55 +0000

    dom

--- js/index.js
+++ js/index.js
@@ -1,34 +1,14 @@
- function add(a, b) {
-     while (a.length < b.length) a.unshift(0);
-     while (a.length > b.length) b.unshift(0);
-     var carry = 0,
-         sum = [];
-     for (var i = a.length - 1; i >= 0; i--) {
-         var s = a[i] + b[i] + carry;
-         if (s >= 10) {
-             s = s - 10;
-             carry = 1;
-         }
-         else {
-             carry = 0;
-         }
-         sum.unshift(s);
-     }
-     if (carry) sum.unshift(carry);
-     return sum;
- }
+function on_btn_gen_click() {
+    count = parseInt(document.getElementById("inCount").value);
+    a = 1, b = 1, sum = 0;
+    output = "<b>The first " + count + " elements in the Fibonacci series:</b> ";
 
-var f1=0,f2=1,f3;
-var i;
-alert("enter a text"+n);
- var n=prompt("enter the number");
- document.write("the fibonacci series is "+"<br/>");
+    for (i = 0; i < count; i++) {
+        output += a + " ";
+        sum = a + b;
+        a = b;
+        b = sum;
+    }
 
-   for(i=2;i<=n;i++)
-   {
-
-       f3=f1+f2;
-       f1=f2;
-       f2=f3;
-     document.write(f3+"<br/>");   
-   }
+    document.getElementById("pSeries").innerHTML = output;
+}
